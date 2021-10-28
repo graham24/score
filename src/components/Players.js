7dde213f31c37630e02bfaf4533129fc0543d10b
graham24 <gdholland01@gmail.com>
2021-10-28 15:48:47 -0600

    add semantic ui and input type number

--- src/components/Players.js
+++ src/components/Players.js
@@ -84,7 +84,7 @@ class PlayerList extends React.Component {
                                         {player.name} - {player.score}
                                     </div>
                                     <form className="add-score" id={player.name} onSubmit={this.addScore}>
-                                        <input type="number" placeholder="Add Score"
+                                        <input type="number" inputMode="numeric" placeholder="Add Score"
                                             onChange={this.updatePlayerScore} />
                                         <button type="submit">Update Score</button>
                                     </form>
